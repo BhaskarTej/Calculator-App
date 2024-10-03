31f68b179a9e6db158b89bd8e167d4dd326db415
BhaskarTej <bhaskarteja02@gmail.com>
2024-10-03 17:32:54 -0400

    Initial Project Setup: Basic Flutter app structure created

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator App'),
+      ),
+      body: Center(
+        child: Text('Welcome to Calculator App!'),
+      ),
+    );
+  }
+}
